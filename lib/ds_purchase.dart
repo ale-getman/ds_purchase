ca9bc898fee231fb59f71d5702e558f7ad6dcfa6
Alexey Tushurashvili <altush@gmail.com>
2024-07-25 18:12:35 +0300

    fix toAppProduct() and toAppPeriod() methods visibility

--- lib/ds_purchase.dart
+++ lib/ds_purchase.dart
@@ -1,5 +1,6 @@
 export 'src/ds_paywall_mixin.dart';
 export 'src/ds_purchase_manager.dart';
 export 'src/ds_purchase_types.dart';
+export 'src/entities/adapty_entities_ext.dart';
 export 'src/entities/ds_paywall_entity.dart';
 export 'src/entities/ds_product_entity.dart';
